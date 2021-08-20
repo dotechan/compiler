a03126a7dc72cbbc1904cc43dcbfc66ae9eb9036
dotechan <this.is.itoooo.0120@gmail.com>
2021-08-20 23:48:24 +0900

    ファイル分割

---
+++ main.c
@@ -0,0 +1,103 @@
+#include "9cc.h"
+
+// Input string
+static char *current_input;
+
+static Token *token;
+
+// エラー箇所を報告する
+void error_tok(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - current_input;
+    fprintf(stderr, "%s\n", current_input);
+    fprintf(stderr, "%*s", pos, " ");   // pos個の空白を出力
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// 新しいトークンを作成してcurに繋げる
+static Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    tok->len = len;
+    cur->next = tok;
+    return tok;
+}
+
+static bool startswith(char *p, char *q) {
+    return memcmp(p, q, strlen(q)) == 0;
+}
+
+// Tokenize 'current_input' and returns new tokens.
+static Token *tokenize(char *p) {
+    current_input = p;
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        // 空白文字をスキップ
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        // Multi-letter punctuator
+        if (startswith(p, "==") || startswith(p, "!=") ||
+            startswith(p, "<=") || startswith(p, ">=")) {
+            cur = new_token(TK_RESERVED, cur, p, 2);
+            p += 2;
+            continue;
+        }
+
+        // Single-letter punctuator
+        if (strchr("+-*/()<>", *p)) {
+            cur = new_token(TK_RESERVED, cur, p++, 1);
+            continue;
+        }
+
+        // Integer literal
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p, 0);
+            char *q = p;
+            cur->val = strtol(p, &p, 10);
+            cur->len = p - q;
+            continue;
+        }
+
+        error_tok(p, "トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p, 0);
+    return head.next;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
+    }
+
+    // トークナイズしてパースする
+    token = tokenize(argv[1]);
+    Node *node = expr(token);
+
+    // アセンブリの前半部分を出力
+    printf(".intel_syntax noprefix\n");
+    printf(".globl main\n");
+    printf("main:\n");
+
+    // 抽象構文木を下りながらコード生成
+    gen(node);
+
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRAXにロードして関数からの返り値とする
+    printf("    pop rax\n");
+    printf("    ret\n");
+    return 0;
+}
---
+++ parse.c
@@ -0,0 +1,157 @@
+#include "9cc.h"
+
+static Token *token;
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進めて
+// 真を返す。それ以外の場合には偽を返す。
+static bool consume(char *op) {
+    if (token->kind != TK_RESERVED ||
+        strlen(op) != token->len ||
+        memcmp(token->str, op, token->len)) {
+        return false;
+    }
+    token = token->next;
+    return true;
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ進める。
+// それ以外の場合にはエラーを報告する。
+static void expect(char *op) {
+    if (token->kind != TK_RESERVED ||
+        strlen(op) != token->len ||
+        memcmp(token->str, op, token->len)) {
+        error_tok(token->str, "'%c'ではありません", op);
+    }
+    token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+static int expect_number() {
+    if (token->kind != TK_NUM) {
+        error_tok(token->str, "数ではありません");
+    }
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+static Node *new_node(NodeKind kind) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    return node;
+}
+
+static Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = new_node(kind);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static Node *new_num(int val) {
+    Node *node = new_node(ND_NUM);
+    node->val = val;
+    return node;
+}
+
+Node *expr(Token *tok);
+static Node *equality();
+static Node *relational();
+static Node *add();
+static Node *mul();
+static Node *unary();
+static Node *primary();
+
+// expr = equality
+Node *expr(Token *tok) {
+    token = tok;
+    return equality();
+}
+
+// equality = relational ("==" relational | "!=" relational)*
+static Node *equality() {
+    Node *node = relational();
+
+    for (;;) {
+        if (consume("==")) {
+            node = new_binary(ND_EQ, node, relational());
+        } else if (consume("!=")) {
+            node = new_binary(ND_NE, node, relational());
+        } else {
+            return node;
+        }
+    }
+}
+
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+static Node *relational() {
+    Node *node = add();
+
+    for (;;) {
+        if (consume("<")) {
+            node = new_binary(ND_LT, node, add());
+        } else if (consume("<=")) {
+            node = new_binary(ND_LE, node, add());
+        } else if (consume(">")) {
+            node = new_binary(ND_LT, add(), node);
+        } else if (consume(">=")) {
+            node = new_binary(ND_LE, add(), node);
+        } else {
+            return node;
+        }
+    }
+}
+
+// add = mul ("+" mul | "-" mul)*
+static Node *add() {
+    Node *node = mul();
+
+    for (;;) {
+        if (consume("+")) {
+            node = new_binary(ND_ADD, node, mul());
+        } else if (consume("-")) {
+            node = new_binary(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+// mul = unary ("*" unary | "/" unary)*
+static Node *mul() {
+    Node *node = unary();
+
+    for (;;) {
+        if (consume("*")) {
+            node = new_binary(ND_MUL, node, unary());
+        } else if (consume("/")) {
+            node = new_binary(ND_DIV, node, unary());
+        } else {
+            return node;
+        }
+    }
+}
+
+// unary = ("+" | "-")? unary | primary
+static Node *unary() {
+    if (consume("+")) {
+        return unary();
+    } else if (consume("-")) {
+        return new_binary(ND_SUB, new_num(0), unary());
+    }
+    return primary();
+}
+
+// primary = "(" expr ")" | num
+static Node *primary() {
+    // 次のトークンが"("なら、"(" expr ")"のはず
+    if (consume("(")) {
+        Node *node = expr(token);
+        expect(")");
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_num(expect_number());
+}
